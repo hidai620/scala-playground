8725518b438ec783805a472861600a69b401c87b
razgriz1 <jet_x@themis.ocn.ne.jp>
2016-02-18 03:10:41 +0900

    clean spec

--- src/test/scala/CakePatternSampleSpec.scala
+++ src/test/scala/CakePatternSampleSpec.scala
@@ -10,23 +10,21 @@ import org.specs2._
  * Created by razgriz1 on 2016/01/16.
  */
 class CakePatternSampleSpec extends Specification with Mockito{
-//  "this is my specification" >> {
-//    "where example 1 must be true" >> {
-//      1 must_== 1
-//    }
-//    "where example 2 must be true" >> {
-//      2 must_== 2
-//    }
-//  }
 
   //モデルのモックの作成
   val usersMock = mock[Users]
-  usersMock.list returns Seq("Tom","Alice","John")
 
 
-  object TestUsersLogic extends UsersLogic with UsersServiceComponent with UsersComponent {
+  object TestUsersLogic
+    extends ProductServiceComponent
+      with UsersServiceComponent
+      with UsersComponent {
+
+    usersMock.list returns Seq("Tom","Alice","John")
+
     val users = usersMock
     val usersService = new UsersService
+    val productService = new ProductService
   }
 
 
@@ -38,20 +36,23 @@ class CakePatternSampleSpec extends Specification with Mockito{
     }
   }
 
-  "UsersLogic spec" >> {
-    "execute must be success." >> {
-      TestUsersLogic.execute must_== 0
-    }
+
+  object FailUsersLogic
+    extends UsersServiceComponent
+      with ProductServiceComponent
+      with UsersComponent {
+
+    val usersServiceMock = mock[UsersService]
+    usersServiceMock.toCSV throws new RuntimeException
+
+    val users = usersMock
+    val usersService = usersServiceMock
+    val productService = new ProductService
   }
 
-  object FailUsersLogic extends UsersLogic with UsersServiceComponent with UsersComponent {
-    val users = usersMock
-    val usersService = mock[UsersService]
-    usersService.toCSV throws new RuntimeException
-  }
   "UsersLogic spec" >> {
     "execute must be faile." >> {
-      FailUsersLogic.execute must throwA[RuntimeException]
+      FailUsersLogic.usersService.toCSV must throwA[RuntimeException]
     }
   }
 }
